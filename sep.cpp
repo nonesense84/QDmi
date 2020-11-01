0ce9084c3d2f0c6cd78cc205e444ea1f6631731d
nonesense84 <nonesense@freenet.de>
2020-11-01 19:57:46 +0100

    Prepared for multithreading. Added max speed of vehicle. Emit speed not as float. Modified order of indicators.

--- sep.cpp
+++ sep.cpp
@@ -1,7 +1,8 @@
 #include "sep.h"
 
-sep::sep()
-{
+sep::sep(){}
+
+void sep::process(){
     udpSocketPzb = new QUdpSocket(this);
     udpSocketPzb->bind(QHostAddress::Any, 10001);
     connect(udpSocketPzb, SIGNAL(readyRead()),this,SLOT(readingPendingPzb()));
@@ -46,14 +47,14 @@ void sep::readingPendingPzb(){
 }
 void sep::readingPendingSpeed(){
     while (udpSocketSpeed->hasPendingDatagrams()) {
-        QVector<quint8> lmsToDecoder(16,0);
+        QVector<quint8> lmsToDecoder(16,0); // FIXME: Den benutze ich doch nicht. Kann weg?
         QByteArray datagram;
         datagram.resize(udpSocketSpeed->pendingDatagramSize());
         udpSocketSpeed->readDatagram(datagram.data(), datagram.size(), &sender, &senderPort);
         if(datagram.size() >= 5){
             quint16 speed = static_cast<quint8>(datagram[0]) +
                            (static_cast<quint8>(datagram[1])<<8);
-            emit newSpeed(static_cast<qreal>(speed));
+            emit newSpeed(speed);
             //qDebug() << "got new speed";
             qint16 forceRel = 0;
             quint8 brakingForceRel = static_cast<quint8>(datagram[3]);
@@ -69,22 +70,24 @@ void sep::readingPendingSpeed(){
 
 void sep::readingPendingMtd(){
     while (udpSocketMtd->hasPendingDatagrams()) {
-        QVector<quint8> lmsToDecoder(12,0);
+        QVector<quint8> lmsToDecoder(13,0);
         QByteArray datagram;
         datagram.resize(udpSocketMtd->pendingDatagramSize());
         udpSocketMtd->readDatagram(datagram.data(), datagram.size(), &sender, &senderPort);
-        if(datagram.size() >= 5){
-            lmsToDecoder[0] = 1;                             // Sifa faulty
-            lmsToDecoder[1] = (datagram[0] >> 4) & 0x0f;     //IndDsd
-            lmsToDecoder[2] = (datagram[0]       & 0x0f) * 2;//IndDsdAcu
-            lmsToDecoder[2] = (datagram[1] >> 4) & 0x0f;     //IndDsdFb
-            lmsToDecoder[6] = (datagram[2] >> 4) & 0x0f;     //IndPhantoUp
-            lmsToDecoder[7] = !((datagram[3] >> 4) & 0x0f);     //IndMtMsO
-            lmsToDecoder[8] =  datagram[3]       & 0x0f;     //IndMtHvtl
-            lmsToDecoder[12]= (datagram[4] >> 4) & 0x0f;     //tractionType     // Muss noch getestet werden!!!!!
-          //lmsToDecoder[9] =  datagram[1]       & 0x0f;     //IndDorTav //Muss noch auf MTD-Seite angepasst werden
+        if(datagram.size() >= 7){
+            lmsToDecoder[1] =   (datagram[0] >> 4) & 0x0f;     //IndDsd
+            lmsToDecoder[2] =   (datagram[0]       & 0x0f) * 2;//IndDsdAcu
+            lmsToDecoder[2] =   (datagram[1] >> 4) & 0x0f;     //IndDsdFb
+            lmsToDecoder[9] =    datagram[1]       & 0x0f;     //IndDorTav //Muss noch auf MTD-Seite angepasst werden
+            lmsToDecoder[6] =   (datagram[2] >> 4) & 0x0f;     //IndPhantoUp
+                                                               //IndPhantoDown
+            lmsToDecoder[7] = !((datagram[3] >> 4) & 0x0f);    //IndMtMsO
+            lmsToDecoder[8] =    datagram[3]       & 0x0f;     //IndMtHvtl
+            lmsToDecoder[12]=   (datagram[4] >> 4) & 0x0f;     //tractionType     // Muss noch getestet werden!!!!!
+            lmsToDecoder[0] =    datagram[4]       & 0x0f;     //Sifa faulty
 
         }
         emit newMtdIndicators(lmsToDecoder);
+        emit newFzgVmaxTacho((static_cast<uint8_t>(datagram[5])) |   (static_cast<quint16>(datagram[6]) << 8));
     }
 }
--- sep.h
+++ sep.h
@@ -9,14 +9,17 @@ class sep : public QObject
     Q_OBJECT
 public:
     sep();
+public slots:
+    void process();
 
 signals:
     //void newLzbIndicators(quint8 lmsToDecoder[16]);
     void newLzbIndicators(QVector<quint8> lmsToDecoder);
     void newLzbValues(QVector<quint8> lmsToDecoder);
-    void newSpeed(qreal speed);
+    void newSpeed(quint16 speed);
     void newPowerAbsolute(qint16 power);
     void newMtdIndicators(QVector<quint8> lmsToDecoder);
+    void newFzgVmaxTacho(quint16 speed);
 
 private:
     QUdpSocket *udpSocketPzb;
@@ -31,7 +34,6 @@ private slots:
     void readingPendingLzb();
     void readingPendingMtd();
     void readingPendingSpeed();
-
 };
 
 #endif // SEP_H
